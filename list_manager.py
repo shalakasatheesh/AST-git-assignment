a3a7d89fb432c5acb58c7074963f120c92ff2048
shalakasatheesh <shalakasateesh@gmail.com>
2020-11-21 00:04:59 +0530

    added functions 5 to 8

--- list_manager.py
+++ list_manager.py
@@ -1,33 +1,37 @@
 # List manager
 
-def random_order(list):
+def random_order(a_list):
     return list
 
-def order_by_increasing_value(list):
+def order_by_increasing_value(a_list):
     '''returns the list ordered by increasing value'''
     pass
 
-def order_by_decreasing_value(list):
+def order_by_decreasing_value(a_list):
     '''returns the list ordered by decreasing value'''
     pass
 
-def reverse_order(list):
+def reverse_order(a_list):
     '''returns the list in reverse order'''
     pass
 
-def stringfy_list(list):
+def stringfy_list(a_list):
     '''returns a list with all elements turned into strings'''
-    pass
+    return str(a_list)
 
-def multiply_list(list, multiple):
+def multiply_list(a_list, a):
     '''returns the list with all elements multipled by the value multiple'''
-    pass
+    a_list = [_*a for _ in a_list]
+    return a_list
 
-def get_highest_value(list):
+def get_highest_value(a_list):
     '''returns the highest value of the list'''
-    pass
+    a_list.sort(reverse=True)
+    return a_list[:1]
 
-def get_lowest_value(list):
+def get_lowest_value(a_list):
     '''returns the lowest value of the list'''
-    pass
+    a_list.sort()
+    return a_list[:1]
 
+print(stringfy_list([1,2,3]))
